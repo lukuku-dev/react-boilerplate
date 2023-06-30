2fda674407c2043b4920fee8e1d62c4e2c9cc4e5
sonangrai <shonahangrae@gmail.com>
2023-06-30 17:20:13 +0545

    conditional config for styled-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import styled from "styled-components";
+
 function App() {
-  return <>Hello</>;
+  return <AppWrapper>Hello</AppWrapper>;
 }
 
 export default App;
+
+const AppWrapper = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { default as react } from "@vitejs/plugin-react";
+import { default as reactSWC } from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command }) => {
+  if (command === "serve") {
+    return {
+      // dev specific config
+      plugins: [
+        react({
+          babel: {
+            plugins: [
+              [
+                "babel-plugin-styled-components",
+                {
+                  displayName: true,
+                  fileName: false,
+                },
+              ],
+            ],
+          },
+        }),
+      ],
+    };
+  } else {
+    // command === 'build'
+    return {
+      // build specific config
+      plugins: [reactSWC()],
+    };
+  }
+});
